0be22e59e62a9d0c64a5339691e55ca131003489
Torham <torhamdev@gmail.com>
2023-07-17 23:21:40 +0330

    Add models and print_banner funxtion and clear_terminal funxtion

---
+++ models/tools.py
@@ -0,0 +1,43 @@
+from rich import print
+from os import system
+from platform import platform
+
+
+def print_banner() -> None:
+    """
+    printing the script banenr with python rich lib
+    """
+
+    banner = """
+    [bold green]
+    ███████████  ███                 █████    █████ ███████████ ███
+    ░░███░░░░░░█ ░░░                 ░░███    ░░███ ░█░░░███░░░█░███
+    ░███   █ ░  ████  ████████    ███████     ░███ ░   ░███  ░ ░███
+    ░███████   ░░███ ░░███░░███  ███░░███     ░███     ░███    ░███
+    ░███░░░█    ░███  ░███ ░███ ░███ ░███     ░███     ░███    ░███
+    ░███  ░     ░███  ░███ ░███ ░███ ░███     ░███     ░███    ░░░ 
+    █████       █████ ████ █████░░████████    █████    █████    ███
+    ░░░░░       ░░░░░ ░░░░ ░░░░░  ░░░░░░░░    ░░░░░    ░░░░░    ░░░ 
+                                                                    
+                                                                    
+
+    [/bold green]                                                                                                                            
+    """
+    print(banner)
+
+
+def clear_terminal() -> None:
+    """
+    clearing terminal by running clear in linux and mac. and cls in windows
+    """
+
+    runing_os = platform()
+
+    if "Windows" in runing_os:
+        system("cls")
+
+    elif "Linux" in runing_os:
+        system("clear")
+
+    elif "macOS" in runing_os:
+        system("clear")
