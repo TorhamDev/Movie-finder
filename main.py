9ed42a48303d7722bcb307068a6c0103051a3761
Torham <torhamdev@gmail.com>
2023-07-17 23:23:04 +0330

    Add some input

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from rich import print
+from rich.prompt import Prompt
+
+from models.tools import print_banner
 from avamovie.scraper import AvaMovieScraper
 
-avamoive = AvaMovieScraper()
+
+print_banner()
 
 
-print(avamoive.get_movie_download_links(""))
+search_param = name = Prompt.ask(":sparkles: Search Movie Name")
+
+scraper = AvaMovieScraper()
+
+print("\n\n:tractor:[deep_pink1] Searching...[/deep_pink1]")
+
+
+search_result = scraper.search_and_extract_data(search_param)
+
+print(search_result)
