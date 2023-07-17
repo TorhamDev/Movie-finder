f45c044227b90f682d31d4ea1882502e5a50fc00
Torham <torhamdev@gmail.com>
2023-07-17 16:30:02 +0330

    Update main test code

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from avamovie.scraper import AvaMovieScraper
 
 avamoive = AvaMovieScraper()
 
-print(avamoive.search_and_extract_data("spider man"))
+print(avamoive.get_movie_download_links("https://avamovie2.info/mavka-the-forest-song-2023/"))
