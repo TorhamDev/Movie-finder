8fd3868d258da81ce30c2a3df3a6f64eb54c4863
Torham <torhamdev@gmail.com>
2023-07-17 22:39:24 +0330

    Add todo and more details

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from avamovie.scraper import AvaMovieScraper
 
 avamoive = AvaMovieScraper()
 
-print(avamoive.get_movie_download_links("https://avamovie2.info/mavka-the-forest-song-2023/"))
+
+print(avamoive.get_movie_download_links(""))
